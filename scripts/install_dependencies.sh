d53da3ab0c0cb86279170be640975cdfdea481ee
Michał Prokop <mprokop@amusys.pl>
2025-04-06 21:06:06 +0200

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 echo "Installing dependencies..."
-cd /home/ec2-user/flask-app
+
+APP_DIR="/home/ec2-user/flask-app"
+
+if [ -d "$APP_DIR" ]; then
+  cd "$APP_DIR"
+else
+  echo "❌ Directory $APP_DIR does not exist!"
+  exit 1
+fi
+
+if ! command -v pip3 &> /dev/null; then
+    echo "Installing pip3..."
+    sudo yum update -y
+    sudo yum install -y python3
+fi
+
 pip3 install -r requirements.txt
